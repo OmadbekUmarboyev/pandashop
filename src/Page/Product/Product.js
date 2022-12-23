63862c456b84f28b596067b2b3c234dd4e02fa92
Omadbek <umarboyevomadbek@gmail.com>
2022-12-23 15:40:06 +0500

    fix

--- src/Page/Product/Product.js
+++ src/Page/Product/Product.js
@@ -1,20 +1,29 @@
 import React from "react";
 import "./Product.css";
 import FilterIcon from "./sliders-h-Regular.png";
+import SearchIcon from "./search-Regular.png";
+import { Data2 } from "./../Home/data/Data2";
 
 export function Product() {
-
   return (
     <>
       <div id="Product">
-        <button
-          onClick={() => {
-            
-          }}
-        >
-          <img src={FilterIcon} alt="" />
-        </button>
-        
+        <div id="Filter">
+          <button id="FilterBTN">
+            <img src={FilterIcon} alt="" />
+          </button>
+          <label id="SearcProduct">
+            <input type="text" name="" placeholder="Nima izlamoqdasiz ?" />
+            <button id="SearchBtn">
+              <img src={SearchIcon} alt="" />
+            </button>
+          </label>
+        </div>
+        <div id="Products">
+          <div>
+            <Data2 />
+          </div>
+        </div>
       </div>
     </>
   );
